a7ea40eec137aa874940e723f1f0a154b5914e53
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-02-22 02:40:31 +0100

    filters: define a macro to generate multiple implementations

--- src/filters.rs
+++ src/filters.rs
@@ -1,50 +1,35 @@
 use rs_es::query::*;
 use rs_es::units::JsonVal;
 
+macro_rules! build_terms_of_array_impl {
+  ($t:ty) => {
+    impl BuildTermsOfArray<$t> for Filter {
+      fn build_terms(key: &'static str, values: &Vec<$t>) -> Vec<Filter> {
+        // An empty `values` does nothing so that you don't need to assert its presence
+        if values.is_empty() {
+          return vec![];
+        }
+
+        let mut terms: Vec<Filter> = Vec::new();
+
+        for value in values {
+          let term = Filter::build_terms(
+            key,
+            vec![JsonVal::from(*value)]
+          ).build();
+
+          terms.push(term);
+        }
+
+        terms
+      }
+    }
+  }
+}
+
 pub trait BuildTermsOfArray<T> {
   fn build_terms(key: &'static str, values: &Vec<T>) -> Vec<Filter>;
 }
 
-impl BuildTermsOfArray<i32> for Filter {
-  fn build_terms(key: &'static str, values: &Vec<i32>) -> Vec<Filter> {
-    // An empty `values` does nothing so that you don't need to assert its presence
-    if values.is_empty() {
-      return vec![];
-    }
-
-    let mut terms: Vec<Filter> = Vec::new();
-
-    for value in values {
-      let term = Filter::build_terms(
-        key,
-        vec![JsonVal::from(*value)]
-      ).build();
-
-      terms.push(term);
-    }
-
-    terms
-  }
-}
-
-impl BuildTermsOfArray<&'static str> for Filter {
-  fn build_terms(key: &'static str, values: &Vec<&'static str>) -> Vec<Filter> {
-    // An empty `values` does nothing so that you don't need to assert its presence
-    if values.is_empty() {
-      return vec![];
-    }
-
-    let mut terms: Vec<Filter> = Vec::new();
-
-    for value in values {
-      let term = Filter::build_terms(
-        key,
-        vec![JsonVal::from(*value)]
-      ).build();
-
-      terms.push(term);
-    }
-
-    terms
-  }
-}
+build_terms_of_array_impl!(i32);
+build_terms_of_array_impl!(&'static str);
