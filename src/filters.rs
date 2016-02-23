9153a11a8da5664faed371b538b2f8f6973f894a
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-02-23 23:51:39 +0100

    *: implement filters for visibility policies

--- src/filters.rs
+++ src/filters.rs
@@ -1,5 +1,42 @@
-use rs_es::query::*;
+use chrono::UTC;
+use chrono::datetime::DateTime;
+
+use company::Company;
+use postgres::Connection;
+
+use rs_es::query::Filter;
 use rs_es::units::JsonVal;
+
+pub fn visibility_filters(conn: &Connection, company_id: &Option<i32>) -> Vec<Filter> {
+  let now = DateTime::timestamp(&UTC::now());
+
+  let visibility_rules = Filter::build_bool()
+                                .with_must(vec![Filter::build_term("accepted", "true")
+                                                      .build(),
+                                                Filter::build_range("batch_starts_at")
+                                                       .with_lt(JsonVal::from(now))
+                                                       .with_format("epoch_second")
+                                                       .build(),
+                                               Filter::build_range("batch_ends_at")
+                                                       .with_gte(JsonVal::from(now))
+                                                       .with_format("epoch_second")
+                                                       .build()]).build();
+  match *company_id {
+    Some(company_id) => {
+      let presented_talent_ids: Vec<i32> = Company::find(conn, &company_id).unwrap().presented_talents;
+
+      let presented_talents = Filter::build_bool()
+                                        .with_must(vec![<Filter as VectorOfTerms<i32>>::build_terms("ids", &presented_talent_ids)].into_iter()
+                                                                                                                                  .flat_map(|x| x)
+                                                                                                                                  .collect::<Vec<Filter>>())
+                                        .build();
+      vec![Filter::build_bool()
+             .with_should(vec![visibility_rules, presented_talents])
+             .build()]
+    },
+    None => vec![visibility_rules]
+  }
+}
 
 macro_rules! build_vector_of_terms_impl {
   ($t:ty) => {
@@ -13,18 +50,12 @@ macro_rules! build_vector_of_terms_impl {
           return vec![];
         }
 
-        let mut terms: Vec<Filter> = Vec::new();
-
-        for value in values {
-          let term = Filter::build_terms(
-            key,
-            vec![JsonVal::from(*value)]
-          ).build();
-
-          terms.push(term);
-        }
-
-        terms
+        vec![Filter::build_terms(
+          key,
+          values.iter()
+                .map(|v| JsonVal::from(*v))
+                .collect::<Vec<JsonVal>>()
+        ).build()]
       }
     }
   }
