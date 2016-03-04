893b2c4d07e6ad0accf33e42b78a80b5351b8e00
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-03-04 01:43:10 +0100

    main: populate indexes via POST and use persistent ES connections

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,9 @@ extern crate chrono;
 mod resources;
 use resources::user::Talent;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::io::Read;
 use std::env;
 
 lazy_static! {
@@ -45,7 +48,8 @@ fn main() {
   println!("Searchspot v{}\n{}\n{}\n", env!("CARGO_PKG_VERSION"), config.es, config.http);
 
   let mut router = Router::new();
-  router.get("/talents", talents);
+  handle_talents_search(&mut router);
+  handle_talents_indexing(&mut router);
 
   let mut chain = Chain::new(router);
 
@@ -57,19 +61,58 @@ fn main() {
   Iron::new(chain).http(&*host).unwrap();
 }
 
-fn talents(req: &mut Request) -> IronResult<Response> {
-  let mut es = Client::new(&*config.es.host, config.es.port);
+fn handle_talents_search(mut router: &mut Router) {
+  let es = Arc::new(Mutex::new(Client::new(&*config.es.host, config.es.port)));
 
-  let params  = req.get_ref::<Params>().ok().unwrap();
-  let indexes = config.es.indexes.iter()
-                                 .map(|e| &**e)
-                                 .collect::<Vec<&str>>();
+  router.get("/talents", move |r: &mut Request| {
+    search_talents(r, &mut es.lock().unwrap(), &*config.es.index)
+  });
+}
 
+fn handle_talents_indexing(mut router: &mut Router) {
+  let es = Arc::new(Mutex::new(Client::new(&*config.es.host, config.es.port)));
+
+  router.post("/talents", move |r: &mut Request| {
+    index_talents(r, &mut es.lock().unwrap(), &*config.es.index)
+  });
+}
+
+macro_rules! try_or_422 {
+  ($expr:expr) => (match $expr {
+    Ok(val)  => val,
+    Err(err) => {
+      let content_type = "application/json".parse::<Mime>().unwrap();
+      let mut error = HashMap::new();
+      error.insert("error", format!("{}", err));
+
+      return Ok(Response::with(
+        (content_type, status::UnprocessableEntity, json::encode(&error).unwrap())
+      ))
+    }
+  })
+}
+
+fn search_talents(req: &mut Request, mut es: &mut Client, index: &str) -> IronResult<Response> {
+  let params   = try_or_422!(req.get_ref::<Params>());
   let response = SearchResult {
-    results: Talent::search(&mut es, indexes, params),
+    results: Talent::search(&mut es, index, params),
     params:  params.clone()
   };
 
   let content_type = "application/json".parse::<Mime>().unwrap();
-  Ok(Response::with((content_type, status::Ok, json::encode(&response.to_json()).unwrap())))
+  Ok(Response::with(
+    (content_type, status::Ok, try_or_422!(json::encode(&response.to_json())))
+  ))
+}
+
+fn index_talents(req: &mut Request, mut es: &mut Client, index: &str) -> IronResult<Response> {
+  let mut payload = String::new();
+  req.body.read_to_string(&mut payload).unwrap();
+
+  let talents: Vec<Talent> = try_or_422!(json::decode(&payload));
+  for talent in talents {
+    try_or_422!(talent.index(es, index));
+  }
+
+  Ok(Response::with(status::Ok))
 }
