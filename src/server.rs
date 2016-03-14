b2c1eb27caec2529d6faeb524743eeb133ecd64f
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-03-15 00:34:52 +0100

    server: encapsulate handlers in their own new structs

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use rustc_serialize::json::{self, ToJson};
 use rs_es::Client;
 
 use iron::prelude::*;
-use iron::status;
+use iron::{status, Handler};
 use iron::mime::Mime;
 
 use logger::Logger;
@@ -44,6 +44,81 @@ pub struct Server<R: Resource> {
   resource: PhantomData<R>
 }
 
+struct SearchableHandler<R> {
+  config:   Config,
+  resource: PhantomData<R>,
+}
+
+impl<R: Resource> SearchableHandler<R> {
+  fn new(config: Config) -> Self {
+    SearchableHandler::<R> { resource: PhantomData, config: config }
+  }
+}
+
+impl<R: Resource> Handler for SearchableHandler<R> {
+  fn handle(&self, req: &mut Request) -> IronResult<Response> {
+    let mut client = Client::new(&*self.config.es.host, self.config.es.port);
+
+    let params   = try_or_422!(req.get_ref::<Params>());
+    let response = SearchResult {
+      results: R::search(&mut client, &*self.config.es.index, params),
+      params:  params.clone()
+    };
+
+    let content_type = "application/json".parse::<Mime>().unwrap();
+    Ok(Response::with(
+      (content_type, status::Ok, try_or_422!(json::encode(&response.to_json())))
+    ))
+  }
+}
+
+struct IndexableHandler<R> {
+  config:   Config,
+  resource: PhantomData<R>,
+}
+
+impl<R: Resource> IndexableHandler<R> {
+  fn new(config: Config) -> Self {
+    IndexableHandler::<R> { resource: PhantomData, config: config }
+  }
+}
+
+impl<R: Resource> Handler for IndexableHandler<R> {
+  fn handle(&self, req: &mut Request) -> IronResult<Response> {
+    let mut client = Client::new(&*self.config.es.host, self.config.es.port);
+
+    let mut payload = String::new();
+    req.body.read_to_string(&mut payload).unwrap();
+
+    let resource: R = try_or_422!(json::decode(&payload));
+    try_or_422!(resource.index(&mut client, &*self.config.es.index));
+
+    Ok(Response::with(status::Created))
+  }
+}
+
+struct ResettableHandler<R> {
+  config:   Config,
+  resource: PhantomData<R>,
+}
+
+impl<R: Resource> ResettableHandler<R> {
+  fn new(config: Config) -> Self {
+    ResettableHandler::<R> { resource: PhantomData, config: config }
+  }
+}
+
+impl<R: Resource> Handler for ResettableHandler<R> {
+  fn handle(&self, _: &mut Request) -> IronResult<Response> {
+    let mut client = Client::new(&*self.config.es.host, self.config.es.port);
+
+    match R::reset_index(&mut client, &*self.config.es.index) {
+      Ok(_)  => Ok(Response::with(status::NoContent)),
+      Err(_) => Ok(Response::with(status::UnprocessableEntity))
+    }
+  }
+}
+
 impl<R: Resource> Server<R> {
   pub fn new(endpoint: String) -> Self {
     let config = match env::args().nth(1) {
@@ -66,74 +141,17 @@ impl<R: Resource> Server<R> {
                                          self.config.http);
 
     let mut router = Router::new();
-    self.handle_search(&mut router);
-    self.handle_indexing(&mut router);
-    self.handle_reset(&mut router);
+    router.get(&self.endpoint,    SearchableHandler::<R>::new(self.config.clone()));
+    router.post(&self.endpoint,   IndexableHandler::<R>::new(self.config.clone()));
+    router.delete(&self.endpoint, ResettableHandler::<R>::new(self.config.clone()));
 
-    let mut chain = Chain::new(router);
-
-    // for some reasons, chain::link makes heroku crash
-    if env::var("DYNO").is_err() {
-      chain.link(Logger::new(None));
-    }
-
-    Iron::new(chain).http(&*host).unwrap();
-  }
-
-  fn handle_search(&self, mut router: &mut Router) {
-    let config = self.config.clone();
-
-    router.get(self.endpoint.clone(), move |r: &mut Request| {
-      let mut es = Client::new(&*config.es.host, config.es.port);
-      Self::search(r, &mut es, &*config.es.index)
-    });
-  }
-
-  fn handle_indexing(&self, mut router: &mut Router) {
-    let config = self.config.clone();
-
-    router.post(self.endpoint.clone(), move |r: &mut Request| {
-      let mut es = Client::new(&*config.es.host, config.es.port);
-      Self::index(r, &mut es, &*config.es.index)
-    });
-  }
-
-  fn handle_reset(&self, mut router: &mut Router) {
-    let config = self.config.clone();
-
-    router.delete(self.endpoint.clone(), move |r: &mut Request| {
-      let mut es = Client::new(&*config.es.host, config.es.port);
-      Self::reset(r, &mut es, &*config.es.index)
-    });
-  }
-
-  fn search(req: &mut Request, mut es: &mut Client, index: &str) -> IronResult<Response> {
-    let params   = try_or_422!(req.get_ref::<Params>());
-    let response = SearchResult {
-      results: R::search(&mut es, index, params),
-      params:  params.clone()
-    };
-
-    let content_type = "application/json".parse::<Mime>().unwrap();
-    Ok(Response::with(
-      (content_type, status::Ok, try_or_422!(json::encode(&response.to_json())))
-    ))
-  }
-
-  fn index(req: &mut Request, mut es: &mut Client, index: &str) -> IronResult<Response> {
-    let mut payload = String::new();
-    req.body.read_to_string(&mut payload).unwrap();
-
-    let resource: R = try_or_422!(json::decode(&payload));
-    try_or_422!(resource.index(es, index));
-
-    Ok(Response::with(status::Created))
-  }
-
-  fn reset(_: &mut Request, mut es: &mut Client, index: &str) -> IronResult<Response> {
-    match R::reset_index(&mut es, index) {
-      Ok(_)  => Ok(Response::with(status::NoContent)),
-      Err(_) => Ok(Response::with(status::UnprocessableEntity))
-    }
+    match env::var("DYNO") { // for some reasons, chain::link makes heroku crash
+      Ok(_)  => Iron::new(router).http(&*host),
+      Err(_) => {
+        let mut chain = Chain::new(router);
+        chain.link(Logger::new(None));
+        Iron::new(chain).http(&*host)
+      }
+    }.unwrap();
   }
 }
