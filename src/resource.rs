269d5f3781086600a4204971b28aaf86033be43e
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-04-28 16:39:05 +0200

    resource: explain what each method listens to

--- src/resource.rs
+++ src/resource.rs
@@ -13,9 +13,16 @@ use std::any::Any;
 use std::fmt::Debug;
 
 pub trait Resource : Send + Sync + Any + Decodable + Debug {
+  /// Respond to GET requests returning an array with found  ids
   fn search(mut es: &mut Client, default_index: &str, params: &Map) -> Vec<u32>;
+
+  /// Respond to POST requests indexing given entity
   fn index(&self, mut es: &mut Client, index: &str) -> Result<IndexResult, EsError>;
+
+  /// Respond to DELETE requests deleting all the entities from given index
   fn reset_index(mut es: &mut Client, index: &str) -> Result<MappingResult, EsError>;
+
+  /// Used internally. TODO: Move outside the trait.
   fn visibility_filters(epoch: &str, presented_talents: Vec<i32>) -> Vec<Filter>;
   fn search_filters(params: &Map, epoch: &str) -> Query;
   fn sorting_criteria() -> Sort;
