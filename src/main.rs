b4a8a78de366e3ce0168d47db9b6ed629c912de7
Giovanni Capuano <webmaster@giovannicapuano.net>
2017-02-04 19:50:41 +0100

    main: remove unused import

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 
-#[macro_use] extern crate lazy_static;
 #[macro_use] extern crate maplit;
 
 extern crate searchspot;
